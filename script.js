32eede06ad1cb5fee46c57e679dd4560a3765741
YagoAyala <yago.ayala@outlook.com>
2022-12-06 21:04:47 -0300

    feat: changed some stuffs

--- script.js
+++ script.js
@@ -1,71 +1,73 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $('.navbar').addClass('sticky');
+    } else {
+      $('.navbar').removeClass('sticky');
+    }
 
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        $('html').css("scrollBehavior", "auto");
-    });
+    if (this.scrollY > 500) {
+      $('.scroll-up-btn').addClass('show');
+    } else {
+      $('.scroll-up-btn').removeClass('show');
+    }
+  });
 
-    $('.navbar .menu li a').click(function(){
-        $('html').css("scrollBehavior", "smooth");
-    });
+  $('.scroll-up-btn').click(function () {
+    $('html').animate({ scrollTop: 0 });
+    $('html').css('scrollBehavior', 'auto');
+  });
 
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+  $('.navbar .menu li a').click(function () {
+    $('html').css('scrollBehavior', 'smooth');
+  });
 
-    var typed = new Typed(".typing", {
-        strings: ["Programador", "Freelancer", "Sonhador"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+  $('.menu-btn').click(function () {
+    $('.navbar .menu').toggleClass('active');
+    $('.menu-btn i').toggleClass('active');
+  });
 
-    var typed = new Typed(".typing-2", {
-        strings: ["Programador", "Freelancer", "Sonhador"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+  var typed = new Typed('.typing', {
+    strings: ['Clinica Estética'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
+  var typed = new Typed('.typing-2', {
+    strings: ['', 'Clinica Estética'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 
-        $('.botao').click(setTimeout(function () {
-        document.getElementById('nome').value=("");
-        document.getElementById('email').value=("");
-        document.getElementById('assunto').value=("");
-        document.getElementById('mensagem').value=("");
-    }, 1000))
+  $('.carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
+
+  $('.botao').click(
+    setTimeout(function () {
+      document.getElementById('nome').value = '';
+      document.getElementById('email').value = '';
+      document.getElementById('assunto').value = '';
+      document.getElementById('mensagem').value = '';
+    }, 1000),
+  );
 });
